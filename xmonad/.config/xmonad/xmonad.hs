50dafafb0de3ca83d18cd933d0ec2d16d2ace6b0
elliott <elliottmv0@gmail.com>
2026-07-07 19:29:15 -0700

    changed browser to firefox and added volume binds

--- xmonad/.config/xmonad/xmonad.hs
+++ xmonad/.config/xmonad/xmonad.hs
@@ -5,13 +5,15 @@ import XMonad.Layout.Spacing
 lh = spacingWithEdge 3 $ layoutHook def
 
 terminalp = "konsole"
-browser = "floorp"
+browser = "firefox"
 
 myKeys =
        [ ("M-e", spawn terminalp)
        , ("M-w", spawn browser)
        , ("M-r", spawn "xmonad --recompile" >> spawn "xmonad --restart")
        , ("M-q", kill)
+       , ("<XF86AudioRaiseVolume>", spawn "amixer -q set Master 5%+")
+       , ("<XF86AudioLowerVolume>", spawn "amixer -q set Master 5%-")
        ]
 
 main = xmonad $ def
